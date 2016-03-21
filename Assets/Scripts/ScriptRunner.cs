b2667bed3985c66ad96a2fbd5f3b2ad089fd55a2
impoqogames <impoqogames@gmail.com>
2016-03-21 20:19:43 +0100

    Iniciado sistema de bloques (respawn)

---
+++ Assets/Scripts/ScriptRunner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptRunner : MonoBehaviour {
+
+	public static float distanceTraveled;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0f, 5f * Time.deltaTime, 0f);
+		distanceTraveled = transform.localPosition.y;
+	}
+}
